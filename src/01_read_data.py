1a613c832b7c308cd7b93d38f25533196bd8cec2
dicaearchus <b.altena@uu.nl>
2024-03-29 10:52:31 +0100

    Update 01_read_data.py
    importing litter picks

--- src/01_read_data.py
+++ src/01_read_data.py
@@ -1,6 +1,16 @@
+import pandas as pd
+import geopandas as gpd
+
 from functions.handler_gpx import gpx_to_traj
 
 GPX_NAME = 'data/Schoonwandelen_714_items.gpx'
 XLS_NAME = 'data/200-records-van-cert-76.xlsx'
 
+# import movement data
 traj = gpx_to_traj(GPX_NAME)
+
+# import picks
+picks = pd.read_excel(XLS_NAME, header=0, index_col=0)
+picks = gpd.GeoDataFrame(picks,
+                         geometry=gpd.points_from_xy(picks.lon, picks.lat),
+                         crs="EPSG:4326")
